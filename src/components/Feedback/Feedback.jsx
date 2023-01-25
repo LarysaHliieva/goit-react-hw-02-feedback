73b8a34ab42e3b3200bc58b96b8467b890457d25
LarysaHliieva <lora.glievaya@gmail.com>
2023-01-26 00:00:47 +0200

    step2 added countTotalFeedback() and countPositiveFeedbackPercentage()

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -15,7 +15,19 @@ class Feedback extends Component {
     this.setState(state => ({ [option]: state[option] + 1 }));
   };
 
+  countTotalFeedback() {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  }
+  countPositiveFeedbackPercentage() {
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    return total ? Math.round((good * 100) / total) : 0;
+  }
+
   render() {
+    const total = this.countTotalFeedback();
+    const positiveFeedback = this.countPositiveFeedbackPercentage();
     return (
       <div className={css.feedback}>
         <h2 className={css.title}>Please leave feedback</h2>
@@ -36,11 +48,13 @@ class Feedback extends Component {
           <h2 className={css.title}>Statistics</h2>
           <ul>
             {feedbackOptions.map(option => (
-              <li className={css.item}>
+              <li key={option} className={css.item}>
                 {option}: {this.state[option]}
               </li>
             ))}
           </ul>
+          <p className={css.item}>total : {total}</p>
+          <p className={css.item}>positive feedback : {positiveFeedback}%</p>
         </div>
       </div>
     );
